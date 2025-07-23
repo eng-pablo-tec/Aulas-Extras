a66953389c9b2b809a5a3ecf917490ec006aecc4
Pablo Henrique Lima dos Santos <136536368+eng-pablo-tec@users.noreply.github.com>
2025-07-23 11:38:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ const dados = {
     { nome: "HTML CSS 1", link: "https://vimeo.com/1090682476?share=copy" }
   ],
   modulo2: [
-    { nome: "(25/07/2025)- Teoria Fundamentos de Teste de Sistemas", link: "#" },
-    { nome: "(25/07/2025)-Testes Unitários", link: "#" },
-    { nome: "(25/07/2025)-Técnicas de Teste", link: "#" }
+    { nome: "(29/04/2025)- Teoria Fundamentos de Teste de Sistemas", link: "#" },
+    { nome: "(29/04/2025)-Testes Unitários", link: "#" },
+    { nome: "(28/04/2025)-Técnicas de Teste", link: "#" }
   ],
   modulo3: [
-    { nome: "(25/07/2025)-Modelagem de Sistemas", link: "#" },
-    { nome: "(25/07/2025)-Scrum", link: "#" },
-    { nome: "(25/07/2025)-Documentação e Planejamento", link: "#" },
-    { nome: "(25/07/2025)-Boas Práticas e Carreira", link: "#" },
-    { nome: "(25/072025)-Integracao POKEAPI", link: "#" }
+    { nome: "(28/04/2025)-Modelagem de Sistemas", link: "#" },
+    { nome: "(28/04/2025)-Scrum", link: "#" },
+    { nome: "(28/04/2025)-Documentação e Planejamento", link: "#" },
+    { nome: "(28/04/2025)-Boas Práticas e Carreira", link: "#" },
+    { nome: "(28/04/2025)-Integracao POKEAPI", link: "#" }
   ],
   modulo4: [
     { nome: "SELENIUM JAVASCRIPT 1", link: "https://drive.google.com/drive/folders/1KlSodjBM_EDbMpJz5-_qrcZRfiHTc8UA?usp=sharing" },
     { nome: "SELENIUM JAVASCRIPT 2", link: "https://docs.google.com/document/d/1Ef7iYP_aH5STHaTyONp6K7ZEX3XRm5IeFT-Z7yFbeVQ/edit?usp=sharing" },
-    { nome: "(25/07/2025)-Cypress", link: "#" },
-    { nome: "(25/07/2025)-Postman Automatizado", link: "#" },
-    { nome: "(25/07/2025)-Cucumber", link: "#" },
-    { nome: "(25/07/2025)-Jest", link: "#" }
+    { nome: "(28/04/2025)-Cypress", link: "#" },
+    { nome: "(28/04/2025)-Postman Automatizado", link: "#" },
+    { nome: "(28/04/2025)-Cucumber", link: "#" },
+    { nome: "(28/04/2025)-Jest", link: "#" }
   ],
   modulo5: [
-    { nome: "(25/07/2025)-Teste Segurança", link: "#" },
+    { nome: "(28/04/2025)-Teste Segurança", link: "#" },
     { nome: "Teste Carga", link: "https://vimeo.com/1090711667?share=copy" },
-    { nome: "(25/07/2025)-Ferramentas de Teste", link: "#" }
+    { nome: "(28/04/2025)-Ferramentas de Teste", link: "#" }
   ]
 };
 
